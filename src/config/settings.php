319f1cb1c2a878fbb8d24bdf01ae5c830007ee56
ProGenji25 <43196235+ProGenji25@users.noreply.github.com>
2020-02-10 16:53:03 -0700

    Initial commit

---
+++ src/config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+// SWITCH THESE BEFORE YOUR LAST PUSH BEFORE TESTING ON YOUR PRODUCTION ENVIRONMENT
+$servername = 'mariadb';
+// $servername = 'localhost';
+
+// THESE NEED TO MATCH THE VALUES IN docker-compose.yml
+$mysql_user = 'admin';
+$mysql_password = 'changeme';
+$mysql_database = 'todo';
+
+?>
